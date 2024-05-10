7c9c34385d2c19eaafb8681b703ab4fed577e9c6
Zoreslav Goral <zgoral_accentiko@genie.health>
2024-05-10 18:29:46 +0300

    fetch books by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,8 +24,9 @@ public_users.get('/isbn/:isbn', function (req, res) {
 
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
-    //Write your code here
-    return res.status(300).json({message: "Yet to be implemented"});
+    const booksArray = Object.values(books);
+    const filteredBooks = booksArray.filter((book) => book.author === req.params.author);
+    return res.send(filteredBooks);
 });
 
 // Get all books based on title
