fd1d6024ac1d69260560ace0d8d17f19cb27d39a
Zoreslav Goral <zgoral_accentiko@genie.health>
2024-05-10 17:32:11 +0300

    add get by isbn api

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,8 +17,10 @@ public_users.get('/', function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
-    //Write your code here
-    return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    const filteredBooks = books.filter(book => book.isbn === isbn);
+
+    res.send(filteredBooks);
 });
 
 // Get book details based on author
