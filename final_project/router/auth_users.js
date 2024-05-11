3653c9b035e4152e9ba552ec202eb418f34b06bb
Zoreslav Goral <zgoral_accentiko@genie.health>
2024-05-11 08:56:30 +0300

    add add modify review

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -56,11 +56,13 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
     return res.status(300).json({message: "Review is required"});
   }
 
+  console.log(req);
+
   books[isbn] = {
     ...books[isbn],
     reviews: {
       ...books[isbn].reviews,
-      [req.data.user.username]: review
+      [req.user.username]: review
     }
   }
 
